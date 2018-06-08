40eb2d48771a574f23c2793dcecf2d43e7d6b1fb
Mitchell Skaggs <skaggsm333@gmail.com>
2018-06-08 11:43:02 -0500

    Added unit tests for zipLiveDataNullable function

--- kotlin-livedata-utils/src/test/java/com/github/magneticflux/livedata/LiveDataTestUtils.kt
+++ kotlin-livedata-utils/src/test/java/com/github/magneticflux/livedata/LiveDataTestUtils.kt
@@ -2,28 +2,18 @@ package com.github.magneticflux.livedata
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.Observer
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
 
 /**
- * Waits a ***very*** short amount of time for a value to be present. Should be used with [InstantTaskExecutorExtension].
+ * Waits a ***very*** short amount of time for a value to be present. Must be used with [InstantTaskExecutorExtension].
  */
 val <T> LiveData<T>.awaitValue: T?
     get() {
-        var data: T? = null
-        val latch = CountDownLatch(1)
 
         lateinit var observer: Observer<T>
-        observer = Observer { o ->
-            data = o
-            latch.countDown()
+        observer = Observer {
             removeObserver(observer)
         }
-
         this.observeForever(observer)
 
-        latch.await(1, TimeUnit.MILLISECONDS)
-
-        @Suppress("UNCHECKED_CAST")
-        return data
+        return this.value
     }
